c69104de934998ff40a21461895e5465a8c4ea2f
Colin Smith <colin@ftsy.co>
2019-02-01 16:15:00 -0800

    Add wrapper for build script

---
+++ build_opencv.sh
@@ -0,0 +1,16 @@
+./platforms/ios/build_framework.py \
+--dynamic \
+ios \
+--without video \
+--without stitching \
+--without objdetect \
+--without calib3d \
+--without features2d \
+--without highgui \
+--without videoio \
+--without gapi \
+--without dnn \
+--without photo \
+--without ml \
+--without imgproc \
+--without flann
